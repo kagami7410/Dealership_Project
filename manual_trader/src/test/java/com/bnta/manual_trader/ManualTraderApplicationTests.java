0bbcc3f438e9eac74ef78a5935246c22b38f79b8
sujangurung <sujangurung@sujangurungsmbp.home>
2022-05-21 13:14:27 +0100

    basic spring boot loaded

---
+++ manual_trader/src/test/java/com/bnta/manual_trader/ManualTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.manual_trader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
