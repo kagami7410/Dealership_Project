435026c7635037d95c75bfcd862e81143edf82ca
Liban Mohamed <libanam7@gmail.com>
2022-05-25 22:46:26 +0100

    removed public tag from CustomerRepo

--- manual_trader/src/main/java/com/bnta/manual_trader/repositories/CustomerRepository.java
+++ manual_trader/src/main/java/com/bnta/manual_trader/repositories/CustomerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    public List<Customer> getCustomerByName(String name);
+    List<Customer> getCustomerByName(String name);
 
 }
