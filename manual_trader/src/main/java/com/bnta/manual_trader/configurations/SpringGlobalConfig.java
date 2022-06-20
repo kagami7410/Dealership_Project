fd361d6ba5d70b9d9a6263db6e175ba8acca40f1
Liban Mohamed <libanam7@gmail.com>
2022-06-20 10:40:57 +0100

    Added configs package

---
+++ manual_trader/src/main/java/com/bnta/manual_trader/configurations/SpringGlobalConfig.java
@@ -0,0 +1,18 @@
+package com.bnta.manual_trader.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+
+}
